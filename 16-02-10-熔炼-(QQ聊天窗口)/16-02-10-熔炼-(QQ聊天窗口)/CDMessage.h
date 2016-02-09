b64ff6ab1acb9c50caa68f3a2f799e6e4508838c
陈栋 <940092687@qq.com>
2016-02-10 01:43:29 +0800

    定义枚举的两种方式
    定义枚举的两种方式
    生成实体类

---
+++ 16-02-10-熔炼-(QQ聊天窗口)/16-02-10-熔炼-(QQ聊天窗口)/CDMessage.h
@@ -0,0 +1,40 @@
+//
+//  CDMessage.h
+//  16-02-10-熔炼-(QQ聊天窗口)
+//
+//  Created by 陈栋 on 16/2/10.
+//  Copyright © 2016年 man. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+//枚举：一组和数字相关的常量
+//定义枚举:第一种方式
+//enum CDMessageType
+//{
+//    CDMessageTypeSelf = 0,
+//    CDMessageTypeOther = 1
+//};
+//typedef enum CDMessageType CDMessageType;
+
+//定义枚举:第二种方式
+typedef enum {
+    CDMessageTypeSelf,
+    CDMessageTypeOther
+} CDMessageType;
+
+@interface CDMessage : NSObject
+
+@property (nonatomic,copy) NSString *text;
+@property (nonatomic,copy) NSString *time;
+@property (nonatomic,assign) CDMessageType type;
+
+- (instancetype) initWithDic:(NSDictionary *)dic;
+
++ (instancetype) messageWithDic:(NSDictionary *)dic;
+
+
++ (NSArray *) messagesList;
+
+@end
---
+++ 16-02-10-熔炼-(QQ聊天窗口)/16-02-10-熔炼-(QQ聊天窗口)/CDMessage.m
@@ -0,0 +1,41 @@
+//
+//  CDMessage.m
+//  16-02-10-熔炼-(QQ聊天窗口)
+//
+//  Created by 陈栋 on 16/2/10.
+//  Copyright © 2016年 man. All rights reserved.
+//
+
+#import "CDMessage.h"
+
+@implementation CDMessage
+
+- (instancetype) initWithDic:(NSDictionary *)dic{
+    if (self = [super init]) {
+        [self setValuesForKeysWithDictionary:dic];
+    }
+    return self;
+}
+
++ (instancetype) messageWithDic:(NSDictionary *)dic{
+    return [[CDMessage alloc] initWithDic:dic];
+}
+
+
++ (NSArray *) messagesList{
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"messages" ofType:@"plist"];
+    
+    NSArray *dicArray = [NSArray arrayWithContentsOfFile:path];
+    
+    NSMutableArray *tmpArray  = [NSMutableArray array];
+    
+    for (NSDictionary *dic in dicArray) {
+        CDMessage *message = [[CDMessage alloc] initWithDic:dic];
+        [tmpArray addObject:message];
+    }
+    
+    return tmpArray;
+}
+
+
+@end

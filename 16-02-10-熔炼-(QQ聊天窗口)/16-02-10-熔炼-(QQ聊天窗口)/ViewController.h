fa43e6449aca06b126ace337118f7b61b5d53782
陈栋 <940092687@qq.com>
2016-02-10 01:08:42 +0800

    新创建QQ聊天窗口项目
    新创建QQ聊天窗口项目

---
+++ 16-02-10-熔炼-(QQ聊天窗口)/16-02-10-熔炼-(QQ聊天窗口)/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  16-02-10-熔炼-(QQ聊天窗口)
+//
+//  Created by 陈栋 on 16/2/10.
+//  Copyright © 2016年 man. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 16-02-10-熔炼-(QQ聊天窗口)/16-02-10-熔炼-(QQ聊天窗口)/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  16-02-10-熔炼-(QQ聊天窗口)
+//
+//  Created by 陈栋 on 16/2/10.
+//  Copyright © 2016年 man. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
